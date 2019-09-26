6850a707b91b0c0e36cfe0070cbdf3263cd87652
Dimakl <dimakid123@gmail.com>
2019-09-26 22:13:58 +0300

    ended Functions list for now

--- src/main/java/exp4j/Functions.java
+++ src/main/java/exp4j/Functions.java
@@ -1,8 +1,22 @@
 package exp4j;
 
+import net.objecthunter.exp4j.function.Function;
 import net.objecthunter.exp4j.operator.Operator;
+import sun.security.krb5.internal.crypto.Des;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Functions {
+
+    public static Map<String, Operator> functions =
+            Collections.unmodifiableMap(new HashMap<String, Operator>() {{
+                put(Designation.NOT.value, not);
+                put(Designation.XOR.value, xor);
+                put(Designation.AND.value, and);
+                put(Designation.OR.value, or);
+            }});
 
     public enum Designation {
         NOT("not"),
@@ -30,10 +44,6 @@ public class Functions {
         }
     }
 
-    private static boolean isArgumentBoolean(int arg) {
-        return arg == 1 || arg == 0;
-    }
-
     public static Operator not =
             new Operator(Designation.NOT.value, 1, false, Precedence.NOT.value) {
         @Override
@@ -58,6 +68,38 @@ public class Functions {
 
             return arg1 ^ arg2;
         }
+    };
+
+    public static Operator and =
+            new Operator(Designation.AND.value, 2, false, Precedence.AND.value) {
+                @Override
+                public double apply(double... args) {
+                    final int arg1 = (int) args[0];
+                    final int arg2 = (int) args[1];
+                    if (!isArgumentBoolean(arg1) || !isArgumentBoolean(arg2)) {
+                        throw new IllegalArgumentException("Not boolean value of input");
+                    }
+
+                    return arg1 & arg2;
+                }
+            };
+
+    public static Operator or =
+            new Operator(Designation.OR.value, 2, false, Precedence.OR.value) {
+                @Override
+                public double apply(double... args) {
+                    final int arg1 = (int) args[0];
+                    final int arg2 = (int) args[1];
+                    if (!isArgumentBoolean(arg1) || !isArgumentBoolean(arg2)) {
+                        throw new IllegalArgumentException("Not boolean value of input");
+                    }
+
+                    return arg1 & arg2;
+                }
+            };
+
+    private static boolean isArgumentBoolean(int arg) {
+        return arg == 1 || arg == 0;
     }
 
 }
