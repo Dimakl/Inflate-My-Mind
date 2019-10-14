9abd9f5fafdc4a2d75486b5131530e92852f6fe3
Dimakl <dimakid123@gmail.com>
2019-10-14 18:58:24 +0300

    initial commit

---
+++ src/main/java/com/inflatemymind/InflateMyMindApplication.java
@@ -0,0 +1,13 @@
+package com.inflatemymind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflateMyMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflateMyMindApplication.class, args);
+	}
+
+}
