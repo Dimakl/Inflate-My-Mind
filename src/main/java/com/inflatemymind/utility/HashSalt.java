359bb7f5427d5eb21812deeae16dcf8361670e16
Dimakl <dimakid123@gmail.com>
2019-10-15 23:33:30 +0300

    add class working with hash

--- src/main/java/com/inflatemymind/utility/HashSalt.java
+++ src/main/java/com/inflatemymind/utility/HashSalt.java
@@ -1,4 +1,25 @@
 package com.inflatemymind.utility;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class HashSalt {
+
+    private static String SALT;
+
+    static {
+        Scanner scanner = null;
+        String pathToSalt = "src/main/resources/salt";
+        try {
+            scanner = new Scanner(new File(pathToSalt));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        SALT = scanner.nextLine();
+    }
+
+    public static String getSALT() {
+        return SALT;
+    }
 }
