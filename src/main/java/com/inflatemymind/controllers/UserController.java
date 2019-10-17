84344e058dba1199995f64a98689cb48172d2ddd
Dimakl <dimakid123@gmail.com>
2019-10-17 23:06:43 +0300

    add getUserById method

--- src/main/java/com/inflatemymind/controllers/UserController.java
+++ src/main/java/com/inflatemymind/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.inflatemymind.controllers;
 import com.inflatemymind.exceptions.ResourceNotFoundException;
 import com.inflatemymind.models.User;
 import com.inflatemymind.repositories.UserRepository;
+import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,7 @@ import javax.validation.Valid;
 import javax.xml.ws.Response;
 import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -24,6 +26,21 @@ public class UserController {
     @GetMapping
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping
+    public ResponseEntity getUserById(@NonNull Long userId) {
+        Optional<User> user = userRepository.findById(userId);
+
+        if (user.isPresent()) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(user);
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("No user with such id");
+        }
     }
 
     @PostMapping
