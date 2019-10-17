cf3dd38c1962055e1607b93f1a693f13140695a1
Dimakl <dimakid123@gmail.com>
2019-10-18 00:22:44 +0300

    add submission service

--- src/main/java/com/inflatemymind/services/SubmissionService.java
+++ src/main/java/com/inflatemymind/services/SubmissionService.java
@@ -1,4 +1,32 @@
 package com.inflatemymind.services;
 
+import com.inflatemymind.models.Submission;
+import com.inflatemymind.repositories.SubmissionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
 public class SubmissionService {
+
+    @Autowired
+    SubmissionRepository submissionRepository;
+
+    public List<Submission> getSubmissions() {
+        return submissionRepository.findAll();
+    }
+
+    public Optional<Submission> getSubmissionById(Long submissionId) {
+        return submissionRepository.findById(submissionId);
+    }
+
+    public List<Submission> getSubmissionsByExpressionId(Long expressionId) {
+        List<Submission> submissions = this.getSubmissions();
+        return submissions.stream()
+                .filter(submission -> submission.getExpressionId().equals(expressionId))
+                .collect(Collectors.toList());
+    }
 }
