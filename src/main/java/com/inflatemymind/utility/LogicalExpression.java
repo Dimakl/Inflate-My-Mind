a70f4ee1bf0b331b61dcdaa07abb04583bad4a26
Dimakl <dimakid123@gmail.com>
2019-10-17 23:56:42 +0300

    add TODO

--- src/main/java/com/inflatemymind/utility/LogicalExpression.java
+++ src/main/java/com/inflatemymind/utility/LogicalExpression.java
@@ -3,6 +3,7 @@ package com.inflatemymind.utility;
 public class LogicalExpression {
 
     public static Boolean expressionIsValid(String expression) {
+        // TODO: add real validation
         return true;
     }
 
