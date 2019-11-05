388e4ab99b9a7f393421b294c338f634dcc7d6a1
Dimakl <dimakid123@gmail.com>
2019-11-05 13:26:53 +0300

    add method for drawing basic elements of scheme

--- src/main/java/com/inflatemymind/utility/ImageFromLogicalExpression.java
+++ src/main/java/com/inflatemymind/utility/ImageFromLogicalExpression.java
@@ -8,25 +8,42 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.Tree;
 
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
 import javax.xml.soap.Node;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
+import java.util.List;
 
 public class ImageFromLogicalExpression {
 
     private String expression;
 
+    private Integer answer;
+
     private List<List<TreeNode>> treeNodes = new ArrayList<>();
 
+    private Image and, or, not;
+
     public static void main(String[] args) {
-        ImageFromLogicalExpression imageFromLogicalExpression = new ImageFromLogicalExpression("(A&B)|(D&Cd)");
+        ImageFromLogicalExpression imageFromLogicalExpression = new ImageFromLogicalExpression("A&B|C", 1);
     }
 
-    public ImageFromLogicalExpression(String expression) {
+    public ImageFromLogicalExpression(String expression, Integer answer) {
         this.expression = expression;
+        this.answer = answer;
         getTreeNodesFromExpression();
-        drawImageFromTreeNodes();
+        try {
+            loadImages();
+            drawImageFromTreeNodes();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
 
     private void getTreeNodesFromExpression() {
         LogicalExpressionLexer lexer = new LogicalExpressionLexer(CharStreams.fromString(expression));
@@ -78,11 +95,83 @@ public class ImageFromLogicalExpression {
                 nextQueue = new LinkedList<>();
             }
         }
+        System.out.println();
     }
 
 
-    private void drawImageFromTreeNodes() {
+    private void drawImageFromTreeNodes() throws IOException {
+        BufferedImage bufferedImage = createBufferedImage();
+        Graphics2D g = (Graphics2D) bufferedImage.getGraphics();
+        g.setColor(Color.BLACK);
+        Stroke stroke = new BasicStroke(3f);
+        g.setStroke(stroke);
+        Font font = new Font("TimesRoman", Font.BOLD, 20);
+        g.setFont(font);
+        drawBasicElements(g);
+        drawConnections(g);
+        ImageIO.write(bufferedImage, "png", new File("coolfile.png"));
     }
+
+    private void drawBasicElements(Graphics2D g) {
+        Integer currentX = 0;
+        Integer currentY = 0;
+        for (int i = treeNodes.size() - 1; i > -1; i--) {
+            for (int j = 0; j < treeNodes.get(i).size(); j++) {
+                TreeNode node = treeNodes.get(i).get(j);
+                node.boxStartX = currentX;
+                node.boxStartY = currentY;
+                switch (node.type) {
+                    case VARIABLE:
+                        String var = node.nodeText;
+                        if (node.isNegated) {
+                            var = "!" + var;
+                        }
+                        g.drawString(var, currentX + 25, currentY + 35);
+                        break;
+                    case PARENS:
+                        if (node.isNegated) {
+                            g.drawImage(not, currentX, currentY, null);
+                        } else {
+                            g.drawLine(currentX, currentY + 25, currentX + 50, currentY + 25);
+                        }
+                        break;
+                    case AND:
+                        g.drawImage(and, currentX, currentY, null);
+                        break;
+                    case OR:
+                        g.drawImage(or, currentX, currentY, null);
+                        break;
+                }
+                g.drawLine(currentX + 75, currentY + 25, currentX + 100, currentY + 25);
+                currentY += 75;
+            }
+            currentY = 0;
+            currentX += 125;
+        }
+    }
+
+    private void drawConnections(Graphics2D g) {
+
+    }
+
+    private BufferedImage createBufferedImage() {
+        int canvasLength = (treeNodes.size() + 1) * 125 - 50; // each block besides the last is 125 and last doesnt contain wires
+        int max = 0;
+        for (List<TreeNode> el : treeNodes) {
+            if (el.size() > max) {
+                max = el.size();
+            }
+        }
+        int canvasWidth = 75 * max - 25; // each block is 50, 25 - padding
+        return new BufferedImage(canvasLength, canvasWidth, BufferedImage.TYPE_INT_ARGB);
+    }
+
+    private void loadImages() throws IOException {
+        and = ImageIO.read(getClass().getResource("/operator_images/and.png"));
+        or = ImageIO.read(getClass().getResource("/operator_images/or.png"));
+        not = ImageIO.read(getClass().getResource("/operator_images/not.png"));
+    }
+
 
     enum NodeType {
         VARIABLE,
@@ -93,15 +182,17 @@ public class ImageFromLogicalExpression {
 
     class TreeNode {
         NodeType type;
-        String NodeToken;
+        String nodeText;
         Boolean isNegated;
         TreeNode next;
+        Integer boxStartX;
+        Integer boxStartY;
 
         // конструктор поменять
         public TreeNode(ParseTree node, Boolean isNegated, NodeType nodeType, TreeNode next) {
             this.type = nodeType;
             this.next = next;
-            this.NodeToken = node.getText();
+            this.nodeText = node.getText();
             this.isNegated = isNegated;
         }
     }
