5f779612213c074d32e792e4976d14dcbcfe0177
Dimakl <dimakid123@gmail.com>
2019-11-05 21:01:23 +0300

    replace static into public

--- src/main/java/com/inflatemymind/configurations/WebConfig.java
+++ src/main/java/com/inflatemymind/configurations/WebConfig.java
@@ -1,9 +1,7 @@
 package com.inflatemymind.configurations;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @EnableWebMvc
@@ -12,5 +10,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/","classpath:/other-resources/");
     }
 }
